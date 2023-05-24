31f92949ea0ccdc9fab3ebc6d4dacbc282dbb872
Jeferson Souza <souzajefison@gmail.com>
2023-05-24 13:38:11 -0300

    ci: configuração incial com preact - 👷‍♂

---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vite'
+import { crx } from '@crxjs/vite-plugin'
+import preact from '@preact/preset-vite'
+import zipPack from 'vite-plugin-zip-pack'
+import manifest from './src/manifest'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  return {
+    build: {
+      emptyOutDir: true,
+      outDir: 'build',
+      rollupOptions: {
+        output: {
+          chunkFileNames: 'assets/chunk-[hash].js',
+        },
+      },
+    },
+    esbuild: {
+      logOverride: { 'this-is-undefined-in-esm': 'silent' },
+    },
+    plugins: [
+      crx({ manifest }),
+      preact(),
+      zipPack({
+        outDir: `package`,
+        inDir: 'build',
+        // @ts-ignore
+        outFileName: `${manifest.short_name ?? manifest.name.replaceAll(' ', '-')}-extension-v${
+          manifest.version
+        }.zip`,
+      }),
+    ],
+  }
+})
