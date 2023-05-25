36d0872fdfe79c383f2ae1f5210aeefbca833b88
Jeferson Souza <souzajefison@gmail.com>
2023-05-25 12:25:53 -0300

    feat: styled components - ✨

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -1,15 +1,12 @@
 import Highlight from '@tiptap/extension-highlight'
 import TaskItem from '@tiptap/extension-task-item'
 import TaskList from '@tiptap/extension-task-list'
-import { EditorContent, useEditor } from '@tiptap/react'
+import { useEditor } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
-import './styles.css'
 import { useCallback, useEffect, useState } from 'preact/hooks'
 import MenuBar from '../MenuBar'
-
+import * as S from './styles'
 export default () => {
-  const [status, setStatus] = useState('connecting')
-
   const editor = useEditor({
     extensions: [
       StarterKit.configure({
@@ -21,12 +18,10 @@ export default () => {
     ],
   })
 
-  // Save current user to localStorage and emit to editor
-
   return (
-    <div className="editor">
-      {editor && <MenuBar editor={editor} />}
-      <EditorContent className="editor__content" editor={editor} />
-    </div>
+    <S.Editor>
+      {editor && <MenuBar {...{ editor }} />}
+      <S.Content {...{ editor }} />
+    </S.Editor>
   )
 }
